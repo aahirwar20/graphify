a6cd8f0f45e96f93e261c896dfe0687b886cebdd
aahirwar20 <aahirwar20@iitk.ac.in>
2022-12-09 23:40:23 +0530

    change css

--- script/graph.js
+++ script/graph.js
@@ -25,7 +25,7 @@ t=document.getElementById("in1").value
 function small_graph(){
 x1=x1+10;
 x2=x2-10;
-t=document.getElementById("in1").value
+var t=document.getElementById("in1").value
  generateData(t, x1, x2, 1);
  plot();
 }
@@ -35,33 +35,60 @@ var plot=function(){
 
  var m=document.getElementById("in1");
  if(m==null){
-   t='x';
+   t="x";
  }
  else{
    t=m.value;
  }
  generateData(t, x1, x2, 1);
-
+ 
  new Chart("myChart", {
    type: "line",
    data: {
      labels: xValues,
      datasets: [{
+       label:t,
+       fontSize:40,
        fill: false,
        pointRadius: 0,
-       borderColor: "rgba(0,0,255,0.5)",
-       data: yValues
+       borderColor: "rgb(250,0,0,0.8)",
+       backgroundColor: "rgb(250,0,0,0.2)",
+       data: yValues,
+       
      }]
    },    
    options: {
-     legend: {display: false},
-     title: {
-       display: true,
-       text: t,
-       fontSize: 16
-     }
-   }
+    responsive: true,
+    title: {
+      display: true,
+      text: t,
+      fontSize:40,
+    },
+    
+    interaction: {
+      intersect: true,
+    },
+    scales: {
+      x: {
+        display: true,
+        title: {
+          display: true
+        }
+      },
+      y: {
+        display: true,
+        title: {
+          display: true,
+          text: 'Value'
+        },
+        suggestedMin: -10,
+        suggestedMax: 200
+      }
+    }
+  }
+     
  });
+ 
  
  }
  setTimeout(plot(),100);
