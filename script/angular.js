e118cb7f0379ba56db0f98f80f0640ab218c527b
aahirwar20 <aahirwar20@iitk.ac.in>
2022-12-09 19:04:51 +0530

    change in format

---
+++ script/angular.js
@@ -0,0 +1,13 @@
+var app =angular.module('myapp',[]);
+app.controller('myCtrl',function($scope,$http,$q){
+v= [{name:"Clasical Calculator",data:"xyz", img:"./img/img1.jpg",link:"./public/c_calculator.htm"},
+    {name:"Programming Calculator",data:"xyz", img:"./img/img1.jpg",link:"./public/Programming.htm"},
+    {name:"Graph ",data:"xyz" , img:"./img/img1.jpg",link:"./public/Graph.htm"},
+    {name:"Tempreture Calculator",data:"xyz", img:"./img/img1.jpg",link:"./public/tempreture.htm"},
+    {name:"Unit Calculator",data:"xyz", img:"./img/img1.jpg",link:"./public/unit.htm"},
+    {name:"Programming Calculator",data:"xyz", img:"./img/img1.jpg"},
+];
+
+$scope.sends=v;
+
+});
